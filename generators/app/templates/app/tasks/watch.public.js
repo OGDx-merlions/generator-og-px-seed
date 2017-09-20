f1bb3873a01f1583608b38f19138fad6a67fc8be
Ezhil Vendhan <ezhilvendhan.vezhavendhan@ge.com>
2017-09-20 14:28:21 +0800

    Add Babel transpile option to seed app

--- generators/app/templates/app/tasks/watch.public.js
+++ generators/app/templates/app/tasks/watch.public.js
@@ -3,9 +3,10 @@
 // -------------------------------------
 //   Task: Watch: Public
 // -------------------------------------
-
+const ES6_SRC = ['public/**/*.es6.js', '!public/bower_components'];
 module.exports = function(gulp) {
   return function() {
+    gulp.watch(ES6_SRC, ['transpile']);
     gulp.watch(['public/elements/**/*.scss', 'public/*.scss'], ['compile:sass']);
     gulp.watch(['./public/_index.html','./public/_index-inline-loading-script.js','./public/index-inline.scss'],
     ['compile:index']);
